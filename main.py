2fcb9388d3283d1560e161a2c24a57e7f185bdc5
suvorovjr <pglavan1998@mail.ru>
2024-08-20 15:32:42 +0300

    add changes main.py

--- main.py
+++ main.py
@@ -4,37 +4,51 @@ from typing import Optional
 from schemas import PagesResponseModel, ResponseAPIModel
 
 
-def fetch(url: str, params: dict) -> dict:
-    response = requests.get(url=url, params=params)
-    return response.json()
+def fetch(method: str, url: str, params: dict) -> dict:
+    if method == 'GET':
+        response = requests.get(url=url, params=params)
+        return response.json()
+    elif method == 'POST':
+        response = requests.post(url=url, params=params)
+        return response.json()
 
 
 def get_api_pages(access_token: str) -> PagesResponseModel:
+    method = 'GET'
     url = f'https://graph.facebook.com/v20.0/me/accounts'
     params = {'access_token': access_token}
-    response = fetch(url=url, params=params)
+    response = fetch(method=method, url=url, params=params)
     pages = PagesResponseModel(**response)
     return pages
 
 
 def get_posts(page_token: str, page_id: str, limit: Optional[int] = 50) -> ResponseAPIModel:
-    url = f'GET https://graph.facebook.com/v20.0/{page_id}/posts'
+    method = 'GET'
+    url = f'https://graph.facebook.com/v20.0/{page_id}/posts'
     params = {'access_token': page_token, 'limit': limit}
-    response = fetch(url=url, params=params)
+    response = fetch(method=method, url=url, params=params)
     posts = ResponseAPIModel(**response)
     return posts
 
 
-def get_reviews(page_token: str, comment_id: str, limit: Optional[int] = 50):
-    url = f'GET https://graph.facebook.com/v20.0/{comment_id}/comments'
+def get_reviews(page_token: str, post_id: str, limit: Optional[int] = 50) -> ResponseAPIModel:
+    method = 'GET'
+    url = f'https://graph.facebook.com/v20.0/{post_id}/comments'
     params = {'access_token': page_token, 'limit': limit}
-    response = fetch(url=url, params=params)
+    response = fetch(method=method, url=url, params=params)
     reviews = ResponseAPIModel(**response)
     return reviews
 
 
-def answer_from_reviews():
-    pass
+def reply_to_comment(review_id: str, message: str, page_token: str) -> dict:
+    method = 'POST'
+    url = f"https://graph.facebook.com/v20.0/{review_id}/comments"
+    params = {
+        "message": message,
+        "access_token": page_token
+    }
+    response = fetch(method=method, url=url, params=params)
+    return response
 
 
 def main():
