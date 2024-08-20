0984559cbb41e48babb7c92ea5c208ff345039e4
suvorovjr <pglavan1998@mail.ru>
2024-08-20 13:12:49 +0300

    add changes in schemas.py

--- schemas.py
+++ schemas.py
@@ -1,3 +1,5 @@
+import json
+
 from pydantic import BaseModel, Field
 from typing import List, Optional
 from datetime import datetime
@@ -21,4 +23,16 @@ class PagingModel(BaseModel):
 
 class ResponseAPIModel(BaseModel):
     data: List[DataModel]
+    paging: PagingModel
+
+
+class PagesDataModel(BaseModel):
+    access_token: str
+    category: str
+    name: str
+    id: str
+
+
+class PagesResponseModel(BaseModel):
+    data: List[PagesDataModel]
     paging: PagingModel
